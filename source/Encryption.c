96cafa0bee15be535ce014209a5aaf0b44555556
Wingous <mikmuk1@gmail.com>
2025-07-15 12:11:45 +0300

    .

--- source/Encryption.c
+++ source/Encryption.c
@@ -10,11 +10,11 @@ VOID XorByInputKey(IN PBYTE p_shellcode, IN SIZE_T s_shellcode, IN PBYTE p_key, 
 	}
 }
 
-void RC4Init(PRC4CONTEXT context, const unsigned char* key, size_t length)
+void RC4Init(PRC4CONTEXT context, const unsigned char* key, size_t sKeyLength)
 {
-	unsigned int i;
-	unsigned int j;
-	unsigned char temp;
+	unsigned int local_main_index;
+	unsigned int local_swap_index;
+	unsigned char temp_byte_holder;
 
 	// Check parameters
 	if (context == NULL || key == NULL) return (void)ERROR_INVALID_PARAMETER;
@@ -23,21 +23,19 @@ void RC4Init(PRC4CONTEXT context, const unsigned char* key, size_t length)
 	context->main_index = 0;
 	context->swap_index = 0;
 
-	for (i = 0; i < 256; i++)
+	for (local_main_index = 0; local_main_index < 256; local_main_index++)
 	{
-		context->stream_key[i] = i;
+		context->stream_key[local_main_index] = local_main_index;
 	}
 
-	
-	for (i = 0, j = 0; i < 256; i++)
+	for (local_main_index = 0, local_swap_index = 0; local_main_index < 256; local_main_index++)
 	{
-		//Randomize the permutations using the supplied key
-		j = (j + context->stream_key[i] + key[i % length]) % 256;
+		local_swap_index = (local_swap_index + context->stream_key[local_main_index] + key[local_main_index % sKeyLength]) % 256;
 
-		//Swap the values of S[i] and S[j]
-		temp = context->stream_key[i];
+
+		temp_byte_holder = context->stream_key[i];
 		context->stream_key[i] = context->stream_key[j];
-		context->stream_key[j] = temp;
+		context->stream_key[j] = temp_byte_holder;
 	}
 
 }
@@ -47,7 +45,7 @@ void RC4Encrypt(
 	PRC4CONTEXT          pContext_t,
 	const unsigned char *pInput,
 	unsigned char       *pOutput,
-	size_t               sLength
+	size_t               sPayloadLength
 )
 {	
 	unsigned int   main_index   = pContext_t->main_index;
@@ -56,7 +54,7 @@ void RC4Encrypt(
 	unsigned char  temp_byte_holder = 0;
 
 	//Core logic
-	while (sLength > 0)
+	while (sPayloadLength > 0)
 	{
 		main_index = ( main_index + 1 ) % 256;
 
@@ -78,7 +76,7 @@ void RC4Encrypt(
 			pOutput++;
 		}
 
-		sLength--;
+		sPayloadLength--;
 	}
 
 	pContext_t->main_index = main_index;
